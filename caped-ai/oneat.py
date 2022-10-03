7926570ace85baed1e70af4e451b112af9b965a1
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-10-03 20:36:29 +0200

    add oneat as base class

---
+++ caped-ai/oneat.py
@@ -0,0 +1,46 @@
+from oneat.NEATModels import NEATLRNet, NEATFocus, NEATFocusPredict, NEATPredict, NEATResNet, NEATTResNet, NEATVollNet
+
+
+
+class NEATTResNet(NEATTResNet):
+    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+        
+        return super().__init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None)
+    
+    
+class NEATVollNet(NEATVollNet):
+    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+        
+        return super().__init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None)
+    
+    
+class NEATLRNet(NEATLRNet):
+    
+    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+        
+        return super().__init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None)
+    
+class NEATFocus(NEATFocus):
+    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+        
+        return super().__init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None)
+    
+    
+class NEATFocusPredict(NEATFocusPredict):
+    
+    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+        
+        return super().__init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None)
+            
+class NEATPredict(NEATPredict):
+    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+        
+        return super().__init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None)
+    
+    
+class NEATResNet(NEATResNet):
+    
+    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+        
+        return super().__init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None)
+                       
