fda548442f5b73b8e8c4ae9a22dfb1f56665d3d0
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-10-03 20:23:12 +0200

    add version and setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+from os import path
+
+_dir = path.dirname(__file__)
+
+
+with open('README.md') as f:
+    long_description = f.read()
+with open(path.join(_dir,'oneat','_version.py'), encoding="utf-8") as f:
+    exec(f.read())
+
+setup(
+    name="oneat",
+
+    version=__version__,
+
+    author='Varun Kapoor',
+    author_email='varun.kapoor@kapoorlabs.org',
+    url='https://github.com/Kapoorlabs-CAPED/CAPED-AI-oneat/',
+    description='Content aware prediction and detection based on artificial intelligence.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        
+        "oneat",
+        "oneat-augmentations",
+        "viz-oneat",
+       
+    ],
+    packages = find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
