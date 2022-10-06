5feb86a79fb238e87bb4715d133d3165c833cab9
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-10-06 16:36:04 +0200

    code improve

--- caped_ai/oneat.py
+++ caped_ai/oneat.py
@@ -4,44 +4,44 @@ from oneat.NEATModels import NEATLRNet, NEATFocus, NEATFocusPredict, NEATPredict
 
 
 class NEATTResNet(NEATTResNet):
-    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+    def __init__(self, config, model_dir, model_name, catconfig, cordconfig):
         
         return super().__init__(self, config, model_dir, model_name,  catconfig=catconfig, cordconfig=cordconfig)
     
     
 class NEATVollNet(NEATVollNet):
-    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+    def __init__(self, config, model_dir, model_name, catconfig, cordconfig):
         
         return super().__init__(self, config, model_dir, model_name,  catconfig=catconfig, cordconfig=cordconfig)
     
     
 class NEATLRNet(NEATLRNet):
     
-    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+    def __init__(self, config, model_dir, model_name, catconfig, cordconfig):
         
         return super().__init__(self, config, model_dir, model_name,  catconfig=catconfig, cordconfig=cordconfig)
     
 class NEATFocus(NEATFocus):
-    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+    def __init__(self, config, model_dir, model_name, catconfig, cordconfig):
         
         return super().__init__(self, config, model_dir, model_name,  catconfig=catconfig, cordconfig=cordconfig)
     
     
 class NEATFocusPredict(NEATFocusPredict):
     
-    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+    def __init__(self, config, model_dir, model_name, catconfig, cordconfig):
         
         return super().__init__(self, config, model_dir, model_name,  catconfig=catconfig, cordconfig=cordconfig)
             
 class NEATPredict(NEATPredict):
-    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+    def __init__(self, config, model_dir, model_name, catconfig, cordconfig):
         
         return super().__init__(self, config, model_dir, model_name,  catconfig=catconfig, cordconfig=cordconfig)
     
     
 class NEATResNet(NEATResNet):
     
-    def __init__(self, config, model_dir, model_name,  catconfig=None, cordconfig=None):
+    def __init__(self, config, model_dir, model_name, catconfig, cordconfig):
         
         return super().__init__(self, config, model_dir, model_name,  catconfig=catconfig, cordconfig=cordconfig)
                        
